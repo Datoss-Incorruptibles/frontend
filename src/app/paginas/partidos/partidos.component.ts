a50b8d384cb4b410182be3f9887ce2160235003d
ecriveracgmail <ecriverac@gmail.com>
2021-01-11 17:52:07 -0500

    get dandidatos json v0.7

--- src/app/paginas/partidos/partidos.component.ts
+++ src/app/paginas/partidos/partidos.component.ts
@@ -13,17 +13,27 @@ export class PartidosComponent implements OnInit {
 
   ngOnInit(): void {
     this.restApi.getOrganizacionPolitica().subscribe(res =>{
-      console.log(res); 
-      this.partidos=res;
+      this.partidos=res;            
+      ////////////
+      this.partidos.forEach(partido => {
+        partido.indicadorCantidadEstudio=0;
+        partido.indicadorCantidadSentencias=0;
+        partido.indicadorCantidadTrayectoria=0;
+        partido.indicadorescategoriaorg.forEach(indicador => {
+          if(indicador.indicador==1) partido.indicadorCantidadEstudio+=indicador.cantidad;
+          if(indicador.indicador==3) partido.indicadorCantidadSentencias+=indicador.cantidad;
+          if(indicador.indicador==5) partido.indicadorCantidadTrayectoria+=indicador.cantidad;
+        });
+      });
+      console.log(this.partidos);
       this.onOrdernar("nombre");
     }, error => {  });
   }
 
   onOrdernar(parametro){
-    console.log(parametro);
+    //console.log(parametro);
     this.partidos=this.partidos.sort(function(a,b){
-      console.log(a[parametro] < b[parametro]);
-      
+      //console.log(a[parametro] < b[parametro]);
         if(a[parametro] < b[parametro]) { return -1; }
         if(a[parametro] > b[parametro]) { return 1; }
       return 0;      

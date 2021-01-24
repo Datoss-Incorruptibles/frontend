38dd06cde3326c71cb33fa856ec6ede541265787
Lout33 <sssimple.33@gmail.com>
2021-01-24 15:04:54 -0500

    remove ids from localstorage in first load, temporaly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'Elecciones2021';
   showFiller=false;
+
+  ngOnInit(): void {
+    // temporal , only to prevent crash with olds values saved in localStorage
+    localStorage.removeItem("ids")
+  }
 }

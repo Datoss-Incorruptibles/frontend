f82d826c7734cb86be43b42a99349c561c469e0a
Daniel Tejeda <20172702@aloe.ulima.edu.pe>
2021-02-09 10:22:12 -0500

    Avance1

--- src/app/paginas/candidato-detalle/candidato-medios/candidato-medios.component.ts
+++ src/app/paginas/candidato-detalle/candidato-medios/candidato-medios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
@@ -12,31 +13,27 @@ export class CandidatoMediosComponent implements OnInit {
   videoEmbedSanitized:any;
 
   constructor(private _sanitizer: DomSanitizer) {}
-  /*
-  ngOnInit(): void {
-  }
-  */
+
   ngOnInit(): void {
 
     this.youtubeEmbed()
   }
   
   youtubeEmbed(): void {
-    
     console.log(this.candidato.jne_idhojavida) 
 
     let medios = this.candidato.medios
-
     /*console.log(medios)*/
 
     if (medios) {
       for (let i = 0; i < medios.length; i++) {
-        /*console.log(medios[i].url);*/
+      
         
         if (medios[i].tipo=="video"){
           let videoURL = medios[i].url
-          let videoId = videoURL.substr(32)
-          console.log(videoId)
+          console.log(medios[i].url)
+          //let videoId = videoURL.substr(32)
+          //console.log(videoId)
 
           // el problema era un tema de seguridad de angular para prevenir XSS :
           // https://stackoverflow.com/questions/38037760/how-to-set-iframe-src-without-causing-unsafe-value-exception
@@ -45,23 +42,20 @@ export class CandidatoMediosComponent implements OnInit {
           // this.videoEmbed = "https://www.youtube.com/embed/"+videoId  
 
 
-          this.videoEmbed = "https://www.youtube.com/embed/"+"2G_NYFJq1ps"
+        //  this.videoEmbed = "https://www.youtube.com/embed/"+"2G_NYFJq1p
+          this.videoEmbed = "https://www.youtube.com/embed/"+this.get_video_id(videoURL)
+          console.log("this.videoEmbed ",this.videoEmbed)
           this.videoEmbedSanitized = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoEmbed)
           console.log(this.videoEmbedSanitized);
-          
-          console.log(this.videoEmbed)
-          console.log("")
 
         }
 
       }
     }
   }
-
-  // getVideoId(url: string): string {
-  //   let video = url.substr(32)
-  //   let videoEmbed = "https://www.youtube.com/embed/"+video
-  //   return videoEmbed
-  // } 
-
+  
+  get_video_id(input: string): string {
+    return input.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=|\/watch\?app=desktop&v=|\/sandalsResorts#\w\/\w\/.*\/))([^\/&]{10,12})/)[1]; 
+  }
+  
 }

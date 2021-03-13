40f5637a571498d5b5358cfac950c0772533d640
Lout33 <sssimple.33@gmail.com>
2021-03-13 12:22:06 -0500

    fix urlTag

--- src/app/paginas/candidato-detalle/candidato-detalle.component.ts
+++ src/app/paginas/candidato-detalle/candidato-detalle.component.ts
@@ -45,7 +45,7 @@ export class CandidatoDetalleComponent implements OnInit {
       /* SEO Stuff */
       let descriptionTag = `NOMBRES:${this.candidato.nombres} ${this.candidato.apellido_paterno} ${this.candidato.apellido_materno} DNI:${this.candidato.documento_identidad}`
       let titleTag = `${this.candidato.nombres} ${this.candidato.apellido_paterno} ${this.candidato.apellido_materno}`
-      let candidatoUrlTag = `https://www.candidatos.pe/candidato/136626/${this.candidato.jne_idhojavida}/`;
+      let candidatoUrlTag = `https://www.candidatos.pe/candidato/${this.candidato.jne_idhojavida}/`;
 
       this.title.setTitle(titleTag);
       this.meta.updateTag({name: "description", content:descriptionTag});

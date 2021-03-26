3c8a3f69f3c65174b7c620ddbf745e2e56a0245d
Lout33 <sssimple.33@gmail.com>
2021-03-26 14:30:19 -0500

    update to version 11 angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,8 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
+    initialNavigation: 'enabled',
+    relativeLinkResolution: 'legacy'
 })],
   exports: [RouterModule]
 })

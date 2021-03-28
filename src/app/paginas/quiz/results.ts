3ed64c2ded1c647023e9d6f662cdcaa564634215
Lout33 <sssimple.33@gmail.com>
2021-03-27 22:12:42 -0500

    fix ods

--- src/app/paginas/quiz/results.ts
+++ src/app/paginas/quiz/results.ts
@@ -3,6 +3,7 @@ export var results = [
     name:'ALIANZA PARA EL PROGRESO',
     id:1257,
     value:0,
+    idCandidato: 136626,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785548/candidatos/alianza_am1koz.bmp',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/136626.jpg',
     idDb:11
@@ -11,6 +12,7 @@ export var results = [
     name:"ACCIÓN POPULAR",
     id:4,
     value:0,
+    idCandidato: 134127,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785550/candidatos/pala_o60uvr.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134127.jpg',
     idDb:1
@@ -19,6 +21,7 @@ export var results = [
     name:"AVANZA PAIS - P. INTEGRACION SOCIAL",
     id:2173,
     value:0,
+    idCandidato: 134943,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785547/candidatos/avanzapais_yvh0s5.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134943.jpg',
     idDb:15
@@ -27,6 +30,7 @@ export var results = [
     name:"DEMOCRACIA DIRECTA",
     id:2191,
     value:0,
+    idCandidato: 134133,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785547/candidatos/democraciadirecta_q6cx6u.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134133.jpg',
     idDb:16
@@ -35,6 +39,7 @@ export var results = [
     name:"FRENTE AMPLIO",
     id:2160,
     value:0,
+    idCandidato: 135595,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785550/candidatos/fa_pkhhkh.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/135595.jpg',
     idDb:14
@@ -43,6 +48,7 @@ export var results = [
     name:"JUNTOS POR EL PERU",
     id:1264,
     value:0,
+    idCandidato: 135605,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785548/candidatos/jp_wgxbun.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/135605.jpg',
     idDb:12
@@ -51,6 +57,7 @@ export var results = [
     name:"FUERZA POPULAR",
     id:1366,
     value:0,
+    idCandidato: 133878,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785549/candidatos/k_kzem4w.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/133878.jpg',
     idDb:13
@@ -59,6 +66,7 @@ export var results = [
     name:"PARTIDO MORADO",
     id:2840,
     value:0,
+    idCandidato:135685 ,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785552/candidatos/morado_co98ul.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/135685.jpg',
     idDb:21
@@ -67,6 +75,7 @@ export var results = [
     name:"PARTIDO NACIONALISTA PERUANO",
     id:179,
     value:0,
+    idCandidato: 137760,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785552/candidatos/olla_aoiqyf.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/137760.jpg',
     idDb:10
@@ -75,6 +84,7 @@ export var results = [
     name:"PARTIDO POLITICO NACIONAL PERU LIBRE",
     id:2218,
     value:0,
+    idCandidato: 137792,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785551/candidatos/lapiz_vxjzhg.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/137792.jpg',
     idDb:17
@@ -83,6 +93,7 @@ export var results = [
     name:"PERU PATRIA SEGURA",
     id:55,
     value:0,
+    idCandidato: 136953,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785549/candidatos/ese_v8u1qu.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/136953.jpg',
     idDb:9
@@ -91,6 +102,7 @@ export var results = [
     name:"PODEMOS PERU",
     id:2731,
     value:0,
+    idCandidato: 137291,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785551/candidatos/podemos_knkzkl.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/137291.jpg',
     idDb:20
@@ -99,6 +111,7 @@ export var results = [
     name:"PARTIDO POPULAR CRISTIANO - PPC",
     id:15,
     value:0,
+    idCandidato: 134976,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613788519/candidatos/ppc_sbe7bx.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134976.jpg',
     idDb:4
@@ -107,6 +120,7 @@ export var results = [
     name:"RENOVACION POPULAR",
     id:22,
     value:0,
+    idCandidato: 134923,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785553/candidatos/renovacion_rkpckq.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134923.jpg',
     idDb:6
@@ -115,6 +129,7 @@ export var results = [
     name:"PARTIDO DEMOCRATICO SOMOS PERU",
     id:14,
     value:0,
+    idCandidato: 134382,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785551/candidatos/somosperu_yglutd.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134382.jpg',
     idDb:3
@@ -123,6 +138,7 @@ export var results = [
     name:"VICTORIA NACIONAL",
     id:21,
     value:0,
+    idCandidato: 137542,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785552/candidatos/victorianacional_m2e21v.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/137542.jpg',
     idDb:5
@@ -131,6 +147,7 @@ export var results = [
     name:"RENACIMIENTO UNIDO NACIONAL",
     id:5,
     value:0,
+    idCandidato: 134119,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785553/candidatos/runa_fvjya0.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134119.jpg',
     idDb:2
@@ -139,6 +156,7 @@ export var results = [
     name:"UNION POR EL PERU",
     id:47,
     value:0,
+    idCandidato: 134910,
     imgPartido: 'https://res.cloudinary.com/dv2q4mh6c/image/upload/c_scale,h_70/v1613785554/candidatos/victoria_nwf0it.jpg',
     imgCandidato: 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134910.jpg',
     idDb:8

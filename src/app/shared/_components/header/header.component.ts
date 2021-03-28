98e360bc07838fde8e1a6865eff77995fbf6a299
Lout33 <sssimple.33@gmail.com>
2021-03-28 02:57:35 -0500

    update share url , and render all routes

--- src/app/shared/_components/header/header.component.ts
+++ src/app/shared/_components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,23 @@ import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  href2
+
+  constructor(private router: Router) { }
   @Output()public sidenavToggle = new EventEmitter();
   @Output()public sidehelpToggle = new EventEmitter();
 
   ngOnInit(): void {
+    this.href2 = window.location.href
+    // this.href = this.router.url;
+    this.router.events.subscribe((event: any) => {
+      this.href2 = window.location.href
+    })
+
+
+    // console.log(this.router.url);
+    // console.log(window.location.href)
+
   }
   public onToggleSidenav = () =>{
     this.sidenavToggle.emit()
